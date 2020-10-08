a4558c733e7d0012a3b741d46b85afab6c9a5204
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-10-07 21:30:21 -0300

    add violação OCP

---
+++ SOLID/2 - OCP/Violacao/DebitoConta.cs
@@ -0,0 +1,32 @@
+﻿namespace SOLID.OCP.Violacao
+{
+    /// <summary>
+    /// Violação do OCP (Open/Closed principle - Princípio de aberto/fechado, uma classe
+    /// deve estar fechada para modificação e aberta para extensão)
+    /// </summary>
+    public class DebitoConta
+    {
+        /// <summary>
+        /// Esta classe viola o OCP, pois cada novo tipo de 
+        /// conta que surgir, é adicionado no Enum TipoConta
+        /// e adicionado um novo if na classe, então ela está sendo
+        /// "modificada" ao invés de estendida
+        /// </summary>
+        /// <param name="valor"></param>
+        /// <param name="conta"></param>
+        /// <param name="tipoConta"></param>
+        public void Debitar(decimal valor, string conta, TipoConta tipoConta)
+        {
+            if (tipoConta == TipoConta.Corrente)
+            {
+                // debita conta corrente
+            }
+
+            if (tipoConta == TipoConta.Poupanca)
+            {
+                // Valida aniversário da conta
+                // Debita conta poupança
+            }
+        }
+    }
+}
