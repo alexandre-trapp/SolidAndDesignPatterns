33e3a26c24cfbcaf4ea389067e8095369fd416cb
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-10-20 18:00:30 -0300

    add id to test

--- DesignPatterns/Creational/Singleton/LoadBalancer.cs
+++ DesignPatterns/Creational/Singleton/LoadBalancer.cs
@@ -14,11 +14,11 @@ namespace DesignPatterns.Singleton
         {
             _servers = new List<Server>
             {
-                new Server { Name = "Server 1", IP = "120.14.220.18" },
-                new Server { Name = "Server 2", IP = "120.14.220.19" },
-                new Server { Name = "Server 3", IP = "120.14.220.20" },
-                new Server { Name = "Server 4", IP = "120.14.220.21" },
-                new Server { Name = "Server 5", IP = "120.14.220.22" }
+                new Server { Id = Guid.NewGuid(), Name = "Server 1", IP = "120.14.220.18" },
+                new Server { Id = Guid.NewGuid(), Name = "Server 2", IP = "120.14.220.19" },
+                new Server { Id = Guid.NewGuid(), Name = "Server 3", IP = "120.14.220.20" },
+                new Server { Id = Guid.NewGuid(), Name = "Server 4", IP = "120.14.220.21" },
+                new Server { Id = Guid.NewGuid(), Name = "Server 5", IP = "120.14.220.22" }
             };
         }
 
--- DesignPatterns/Creational/Singleton/Server.cs
+++ DesignPatterns/Creational/Singleton/Server.cs
@@ -1,8 +1,11 @@
-﻿namespace DesignPatterns.Singleton
+﻿using System;
+
+namespace DesignPatterns.Singleton
 {
     internal class Server
     {
         public string Name { get; set; }
         public string IP { get; set; }
+        public Guid Id { get; set; }
     }
 }
